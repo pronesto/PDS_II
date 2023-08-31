93de19f610f87eef4204602aaa83f8484a989145
Fernando Quintao Pereira <pronesto@gmail.com>
2023-08-31 18:38:29 -0300

    Adding new lecture notes.

---
+++ Aulas/09_05_Controle/src/L0.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int factF(int n) {
+  int a = 1;
+  for (int i = 2; i <= n; i++) {
+    a *= i;
+  }
+  return a;
+}
+
+int factW(int n) {
+  int a = 1;
+  while (n > 1) {
+    a *= n;
+    n--;
+  }
+  return a;
+}
+
+int factDoW(int n) {
+  if (n < 1) {
+    return 1;
+  } else {
+    int a = 1;
+    do {
+      a *= n;
+      n--;
+    } while (n > 1);
+    return a;
+  }
+}
+
+int main() {
+  int n;
+  std::cin >> n;
+  std::cout << factF(n) << std::endl;
+  std::cout << factW(n) << std::endl;
+  std::cout << factDoW(n) << std::endl;
+}
